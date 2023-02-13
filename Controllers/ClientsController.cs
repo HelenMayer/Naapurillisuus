f63d9f4216e45f14e432c9320d24a17bcd3cc397
HelenMayer <you@example.com>
2023-02-13 13:01:26 +0200

    add database and it works

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -1,18 +1,23 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Naapurillisuus.Data;
+
 namespace Naapurillisuus.Controllers;
 
 [ApiController]
 [Route("api/Client")]
 public class ClientsController : ControllerBase
 {
+  private readonly DataContext _context;
+  public ClientsController(DataContext context)
+    {
+        _context = context;
+    }
 
     [HttpGet] 
-    public IEnumerable<Client> Get()
+    public async Task<ActionResult<List<Client>>> getClients()
     {
-        return new []
-        {
-            new Client {firstName = "Ella", lastName = "Mäkki"}
-        };
+         return Ok(await _context.Clients.ToListAsync());
     }
 }

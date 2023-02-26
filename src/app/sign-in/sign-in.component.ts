86b73809a0832e313ef7df80d3f0534aa2fab1bb
HelenMayer <you@example.com>
2023-02-26 13:25:04 +0200

    dont add empty data and return role in signin

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Client } from '../Models/Client';
+import { ClientService } from '../services/client.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,6 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+
+  clients: Client [] = []
+
+  constructor(private ClientService : ClientService){}
+
+  ngOnInit(): void{
+    this.ClientService.getClients().subscribe((result : Client[]) => (this.clients = result));
+  }
+
+  LoginAccess(){
+    let email = document.getElementsByTagName("input")[0].value;
+    let password = document.getElementsByTagName("input")[1].value;
+    console.log(this.clients)
+    console.log(this.clients.find(searchClient => searchClient.email == email).lastName)
+    console.log(this.clients.find(searchClient => searchClient.password == password).lastName)
+
+    if (this.clients.find(searchClient => searchClient.email == email).lastName == this.clients.find(searchClient => searchClient.password == password).lastName ) {
+      let role = this.clients.find(searchClient => searchClient.email == email).role
+    console.log(role)
+  }
+    else{
+    let modal = document.getElementById("modalWindowWarning");
+    modal.style.display = "block"; 
+    document.getElementsByTagName("form")[0].style.opacity = "0.5";
+  }
+}
+
+  onCloseModalWindow(){
+  let modal = document.getElementById("modalWindowWarning");
+  modal.style.display = "none"; 
+  document.getElementsByTagName("form")[0].style.opacity = "1";
+}
 
 }
 
